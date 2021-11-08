3186879595c0390b7e45ca4d8a1909a7a346eabb
COWZinnSPACE <92816614+COWZinnSPACE@users.noreply.github.com>
2021-11-08 14:16:27 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,41 @@
+basic.forever(function () {
+    if (input.lightLevel() > 128) {
+        basic.showString("hi :>")
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
