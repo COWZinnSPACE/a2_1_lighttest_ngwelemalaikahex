777eb9b80700c0fe51d687a4bbae2f8b62ea7d80
COWZinnSPACE <92816614+COWZinnSPACE@users.noreply.github.com>
2021-11-16 13:47:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,62 @@
 basic.forever(function () {
+    // if it's sunny it'll say good morning if not a moon will scroll across the screen
     if (input.lightLevel() > 128) {
-        basic.showString("hi :>")
+        basic.showString("GOOD MORNING")
     } else {
         basic.showLeds(`
-            . . # . .
-            . # . # .
-            # . . . #
-            . # . # .
-            . . # . .
-            `)
-        basic.showLeds(`
             . . . . .
-            . . # . .
-            . # . # .
-            . . # . .
+            # . . . .
+            . . . . .
+            # . . . .
             . . . . .
             `)
         basic.showLeds(`
+            # . . . .
+            . # . . .
             . . . . .
-            . . # . .
-            . # # # .
-            . . # . .
-            . . . . .
+            . # . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            # # . . .
+            # . # . .
+            # . . . .
+            # . # . .
+            # # . . .
+            `)
+        basic.showLeds(`
+            . # # . .
+            # # . # .
+            # # . . .
+            # # . # .
+            . # # . .
+            `)
+        basic.showLeds(`
+            . . # # .
+            . # # . #
+            . # # . .
+            . # # . #
+            . . # # .
+            `)
+        basic.showLeds(`
+            . . . # #
+            . . # # .
+            . . # # .
+            . . # # .
+            . . . # #
+            `)
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . . #
             `)
         basic.showLeds(`
             . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
+            . . . . #
+            . . . . #
+            . . . . #
             . . . . .
             `)
         basic.showLeds(`
