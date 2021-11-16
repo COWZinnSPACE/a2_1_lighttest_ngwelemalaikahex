66b2854175930e7355a2a31b4066b9ff75112f19
COWZinnSPACE <92816614+COWZinnSPACE@users.noreply.github.com>
2021-11-16 14:06:08 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
+let photocell = 0
 basic.forever(function () {
+    photocell = pins.analogReadPin(AnalogPin.P1)
     // if it's sunny it'll say good morning if not a moon will scroll across the screen
-    if (input.lightLevel() > 128) {
+    if (photocell > 900) {
         basic.showString("GOOD MORNING")
     } else {
         basic.showLeds(`
